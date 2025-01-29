721d5e0ed62b5b05a9add826f26b3ed40b15918a
overlord999 <overlord124@gmail.com>
2025-01-29 17:52:14 +0200

    Added animation

--- js/modal.js
+++ js/modal.js
@@ -3,16 +3,7 @@ const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 // Ensure modal is closed on page load (Improved, need to change)
-document.addEventListener('DOMContentLoaded', () => {
-  modal.classList.add('is-hidden'); // Add the hidden class to the modal
-});
-
-// Function to toggle modal visibility (Improved, need to change)
-const toggleModal = () => {
-  modal.classList.toggle('is-hidden');
-};
-
-//const toggleModal = () => modal.classList.toggle('is-hidden');
+const toggleModal = () => modal.classList.toggle('is-hidden');
 
 // Event listeners for open and close buttons
 modalBtnOpen.addEventListener('click', toggleModal);

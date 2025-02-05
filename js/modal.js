5646ce054667fdd4ccacab2a089f9259757c1541
overlord999 <overlord124@gmail.com>
2025-02-05 20:57:44 +0200

    Improved scss files, improved structure

--- js/modal.js
+++ js/modal.js
@@ -8,6 +8,3 @@ const toggleModal = () => modal.classList.toggle('is-hidden');
 // Event listeners for open and close buttons
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
-
-
-
